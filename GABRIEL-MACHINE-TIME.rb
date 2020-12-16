ada94afa8e62a87a239cf42ef59e05384cbb4471
Gabriel Conte Colet <51894156+Gatriel@users.noreply.github.com>
2020-12-16 00:44:04 -0300

    Update GABRIEL-MACHINE-TIME.rb

--- GABRIEL-MACHINE-TIME.rb
+++ GABRIEL-MACHINE-TIME.rb
@@ -1,60 +1,37 @@
-################################################### TIME MACHINE ###############################################################
-
-
+################################################### TIME MACHINE #############################################################
+#GABRIEL-MACHINE-TIME  00:41:26-16/12/2020 
+#this programs aims to parse a json file and rewrite it into a desired format
 require "json"
 
-file = open("payload.json") #receives the JSON file
-payload = file.read #reads the file and puts into a string
+file = open("payload.json") #receives and opens the JSON file
+payload = file.read #reads the desired file and puts into a string
 
 parsed = JSON.parse(payload) #parses the string
 
 #Parses the json file into the desired format
-puts ("{\n \"id\": " +   parsed["id"].to_s +
-      "\n \"recorded_at\": "+  "\""+ parsed["recorded_at"]+"\""+
-      "\n \"status\": "+"\""+ parsed["status"]+"\""+
-      "\n \"machine\": {"+
-      "\n   \"id\": "+ parsed["machine_id"].to_s+
-      "\n },"+
-      "\n \"event\": {"+
-      "\n   \"id\": "+ parsed["event"]["id"].to_s+
-      "\n   \"event_name\": "+"\""+ parsed["event"]["event_name"]+"\""+
-      "\n   \"event_date\": "+"\""+ parsed["event"]["event_date"]+"\""+
-      "\n   \"contact_name\": "+"\""+ parsed["event"]["contact_name"]+"\""+
-      "\n },"+
-      "\n \"file\": {"+
-      "\n   \"position\": "+ parsed["video_sequence"].to_s+
-      "\n   \"compression\": "+"\""+ parsed["file_compression"]+"\""+
-      "\n   \"type\": "+ "\""+parsed["file_type"]+"\""+
-      "\n   \"name\": "+ "\""+parsed["file_name"]+"\""+
-      "\n   \"length\": "+ "\""+parsed["video_length"].to_s+"\""+
-      "\n},"+
-      "\n\"machine\": {"+
-      "\n   \"id\": "+ parsed["machine_id"].to_s+
-      "\n }"+
-      "\n}")
 
-      File.open("exit.json","w") do |file| #saves the parse result into a file
-        file.write("{\n \"id\": " +   parsed["id"].to_s +
-              "\n \"recorded_at\": "+  "\""+ parsed["recorded_at"]+"\""+
-              "\n \"status\": "+"\""+ parsed["status"]+"\""+
-              "\n \"machine\": {"+
-              "\n   \"id\": "+ parsed["machine_id"].to_s+
-              "\n },"+
-              "\n \"event\": {"+
-              "\n   \"id\": "+ parsed["event"]["id"].to_s+
-              "\n   \"event_name\": "+"\""+ parsed["event"]["event_name"]+"\""+
-              "\n   \"event_date\": "+"\""+ parsed["event"]["event_date"]+"\""+
-              "\n   \"contact_name\": "+"\""+ parsed["event"]["contact_name"]+"\""+
-              "\n },"+
-              "\n \"file\": {"+
-              "\n   \"position\": "+ parsed["video_sequence"].to_s+
-              "\n   \"compression\": "+"\""+ parsed["file_compression"]+"\""+
-              "\n   \"type\": "+ "\""+parsed["file_type"]+"\""+
-              "\n   \"name\": "+ "\""+parsed["file_name"]+"\""+
-              "\n   \"length\": "+ "\""+parsed["video_length"].to_s+"\""+
-              "\n },"+
-              "\n\"machine\": {"+
-              "\n   \"id\": "+ parsed["machine_id"].to_s+
-              "\n }"+
-              "\n}")
-      end
+File.open("exit.json","w") do |file| #saves the parse result into a file
+  file.write("{\n \"id\": " +   parsed["id"].to_s +
+    "\n \"recorded_at\": "+  "\""+ parsed["recorded_at"]+"\""+
+     "\n \"status\": "+"\""+ parsed["status"]+"\""+
+     "\n \"machine\": {"+
+     "\n   \"id\": "+ parsed["machine_id"].to_s+
+     "\n },"+
+     "\n \"event\": {"+
+     "\n   \"id\": "+ parsed["event"]["id"].to_s+
+     "\n   \"event_name\": "+"\""+ parsed["event"]["event_name"]+"\""+
+     "\n   \"event_date\": "+"\""+ parsed["event"]["event_date"]+"\""+
+     "\n   \"contact_name\": "+"\""+ parsed["event"]["contact_name"]+"\""+
+     "\n },"+
+     "\n \"file\": {"+
+     "\n   \"position\": "+ parsed["video_sequence"].to_s+
+     "\n   \"compression\": "+"\""+ parsed["file_compression"]+"\""+
+     "\n   \"type\": "+ "\""+parsed["file_type"]+"\""+
+     "\n   \"name\": "+ "\""+parsed["file_name"]+"\""+
+     "\n   \"length\": "+ "\""+parsed["video_length"].to_s+"\""+
+     "\n },"+
+     "\n\"machine\": {"+
+     "\n   \"id\": "+ parsed["machine_id"].to_s+
+     "\n }"+
+     "\n}")
+end
